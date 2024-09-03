c6d69623a568f0a2e9ff5d7c5f7604f5430a52f1
CodyKoInABox <codykoinabox@gmail.com>
2024-09-03 16:34:03 -0300

    create basic (but working) db POST testing page

--- bonsite/src/app/testing/database/post/page.tsx
+++ bonsite/src/app/testing/database/post/page.tsx
@@ -1,13 +1,75 @@
+import { db } from '@/database/db';
 import '../../style.css'
+import './style.css'
+import { bonsaiTable } from '@/database/schema';
 
 
 export default function Page() {
     
     return (
             
-        <div>
-            <h1>POST work in progress</h1>
+        <div className='fullPageHorizontalCenterFlex'>
+            
+            <div className='text-center mt-10 bg-gray-400 p-10'>
+
+                <h3 className='font-semibold'>POST | Create Bonsai</h3>
+
+                <div className='w-full'>
+
+                    <form 
+                    className='dbTestInputForm'
+                    action={async (formData) => {
+
+                        'use server'
+
+                        const id = formData.get('id');
+                        const name = formData.get('name');
+                        const amount = formData.get('amount');
+
+                        await db.insert(bonsaiTable).values({
+                            id: Number(id),
+                            name: String(name),
+                            amount: Number(amount),
+                        })
+
+                    }}>
+
+                        <label>
+                            ID:
+                            <input type='number' name='id'></input>
+                        </label>
+
+                        <label>
+                            Name:
+                            <input type='text' name='name'></input>
+                        </label>
+
+                        <label>
+                            Amount:
+                            <input type='number' name='amount'></input>
+                        </label>
+
+                        <button className='simpleButton'>Submit</button>
+
+                    </form>
+
+                </div>
+
+            </div>
+
         </div>
 
     );
 }
+
+
+{ <form action={async () => {
+    'use server'
+    await db.insert(bonsaiTable).values({
+        id: 21,
+        name: 'Ipe',
+        amount: 2
+    })
+}}>
+    <button>Submit</button>
+</form> }
