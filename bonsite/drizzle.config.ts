9c149bb05d74cb73c025578b6188721c07bbca6b
CodyKoInABox <codykoinabox@gmail.com>
2024-09-03 16:09:05 -0300

    more test database configs

--- bonsite/drizzle.config.ts
+++ bonsite/drizzle.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "drizzle-kit";
 config({ path: '.env' });
 
 export default defineConfig({
-  schema: "./src/schema.ts",
+  schema: "./src/database/schema.ts",
   out: "./migrations",
   dialect: "postgresql",
   dbCredentials: {
