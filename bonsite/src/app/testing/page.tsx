8578b9fd9b120bfbc6f0f5b83cf3dfc5a4378ec4
CodyKoInABox <codykoinabox@gmail.com>
2024-09-03 03:35:27 -0300

    fix landing pages

--- bonsite/src/app/testing/page.tsx
+++ bonsite/src/app/testing/page.tsx
@@ -1,16 +1,16 @@
 export default function Page() {
     
     return (
-            
+        
         <div>
-            
+
             <div className="text-center w-screen font-mono mt-20">
                 <h1>Bonsite | Dev</h1>
                 <h2>Database Testing</h2>
             </div>
-        
+
             <div className="text-center w-screen font-mono font-semibold mt-5">
-                <h2>Go to /post or /get to test the database API connection.</h2>
+                <h2>Go to /database to test the database API connection.</h2>
             </div>
 
         </div>
