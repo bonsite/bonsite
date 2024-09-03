4071e7b272ee4d395f2c6c6e1bb67b839bcf0128
CodyKoInABox <codykoinabox@gmail.com>
2024-09-03 15:01:50 -0300

    add universal layout to testing route

--- bonsite/src/app/testing/database/page.tsx
+++ bonsite/src/app/testing/database/page.tsx
@@ -4,11 +4,6 @@ export default function Page() {
             
         <div>
             
-            <div className="text-center w-screen font-mono mt-20">
-                <h1>Bonsite | Dev</h1>
-                <h2>Database Testing</h2>
-            </div>
-        
             <div className="text-center w-screen font-mono font-semibold mt-5">
                 <h2>Go to /post or /get to test the database API connection.</h2>
             </div>
---
+++ bonsite/src/app/testing/layout.tsx
@@ -0,0 +1,27 @@
+import "../..//app/globals.css";
+
+export const metadata = {
+  title: 'Bonsite | Testing',
+  description: 'Testing area of Bonsite'
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+
+    <div>
+
+        <div className="text-center w-screen font-mono mt-20">
+            <h1>Bonsite | Dev</h1>
+            <h2>Testing Area</h2>
+        </div>
+
+        {children}
+
+    </div>
+
+  );
+}
