48683725d305017360f917cffc34791278d7f5af
CodyKoInABox <codykoinabox@gmail.com>
2024-09-26 14:55:21 -0300

    change folder location

--- bonsite/drizzle.config.ts
+++ bonsite/drizzle.config.ts
@@ -5,7 +5,7 @@ config({ path: '.env' });
 
 export default defineConfig({
   schema: "./src/database/schema.ts",
-  out: "./migrations",
+  out: "./database/migrations",
   dialect: "postgresql",
   dbCredentials: {
     url: process.env.DATABASE_URL!,
