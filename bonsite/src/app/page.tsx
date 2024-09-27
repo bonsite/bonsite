f0c21554c45cdce38a94365407ceb8d6e4a958d3
CodyKoInABox <codykoinabox@gmail.com>
2024-09-27 17:09:24 -0300

    create navbar

--- bonsite/src/app/page.tsx
+++ bonsite/src/app/page.tsx
@@ -1,11 +1,14 @@
 "use client"
 
 import Product from '@/components/Store/ProductList';
+import Navbar from '@/components/Store/Header/Navbar'
 
 
 export default function Page() {
     return <>
 
+      <Navbar></Navbar>
+      
       <Product></Product>
 
 
