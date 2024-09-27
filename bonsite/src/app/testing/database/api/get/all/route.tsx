3549fba36645316762fa40c019c0331e227a4022
E-Quino App <equino.git.app@gmail.com>
2024-09-26 21:27:11 -0300

    Update route.tsx
    THIS IS REALLY A MESS

--- bonsite/src/app/testing/database/api/get/all/route.tsx
+++ bonsite/src/app/testing/database/api/get/all/route.tsx
@@ -1,18 +1,13 @@
 import { NextResponse } from "next/server";
 import { getAllBonsais } from "../../../_queries/get"
 
+export const revalidate = 0;
+export const dynamic = "force-dynamic";
+
 export async function GET() {
-    try {
-        console.log('Fetching all bonsai...');
-        const bonsai = await getAllBonsais();
-        console.log('Fetched bonsai list:', bonsai);
-        return NextResponse.json({ success: true, data: bonsai }, { headers: { 'Cache-Control': 'no-store' } });
-    } catch (error) {
-        console.error('Error fetching all bonsai:', error);
-        let errorMessage = 'An error occurred while fetching bonsai';
-        if (error instanceof Error) {
-            errorMessage = error.message;
-        }
-        return NextResponse.json({ success: false, error: errorMessage });
-    }
+
+        let bonsai = await getAllBonsais();
+        
+        return NextResponse.json(bonsai);
 }
+
