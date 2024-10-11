84665c8909918852790def7a35a9e65ba902fa4b
CodyKoInABox <codykoinabox@gmail.com>
2024-10-11 15:23:24 -0300

    navbar is now on layout (universal to all pages)

--- bonsite/src/app/page.tsx
+++ bonsite/src/app/page.tsx
@@ -1,13 +1,10 @@
 "use client"
 
 import ProductList from '@/components/Store/Product/ProductList';
-import Navbar from '@/components/Store/Header/Navbar'
 
 
 export default function Page() {
     return <>
-
-      <Navbar></Navbar>
       
       <ProductList></ProductList>
 
