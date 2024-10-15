c1088ae2d914bfe9c63f2d958119fe4a1f5fe9c5
CodyKoInABox <codykoinabox@gmail.com>
2024-10-15 12:28:24 -0300

    trying to fix drizzle nothing works

--- src/database/migrations/0003_blushing_talisman.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE "schema"."bonsais" ADD COLUMN "visivel" boolean DEFAULT true NOT NULL;--> statement-breakpoint
-ALTER TABLE "schema"."bonsais" DROP COLUMN IF EXISTS "visible";
--- src/database/migrations/0004_optimal_celestials.sql
+++
@@ -1,4 +0,0 @@
-DROP TABLE "schema"."bonsais";--> statement-breakpoint
-DROP TABLE "schema"."clientes";--> statement-breakpoint
-DROP TABLE "schema"."pedidos";--> statement-breakpoint
-DROP SCHEMA "schema";
