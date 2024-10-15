7cd53af4f3b519ca51e8c20edebc5831c98fee82
CodyKoInABox <codykoinabox@gmail.com>
2024-10-15 14:44:13 -0300

    support for nextUI but it doesnt work

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,6 +5,7 @@ const config: Config = {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
     extend: {
@@ -18,6 +19,7 @@ const config: Config = {
       },
     },
   },
+  darkMode: "class",
   plugins: [
     require('@tailwindcss/aspect-ratio'),
   ],
