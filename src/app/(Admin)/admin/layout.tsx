55b7d88a8225bbe220beb77d8f8eaa2636840c26
CodyKoInABox <codykoinabox@gmail.com>
2024-10-15 14:48:36 -0300

    nextUI should be fixed now

--- src/app/(Admin)/admin/layout.tsx
+++ src/app/(Admin)/admin/layout.tsx
@@ -1,3 +1,7 @@
+import {Providers} from "../../providers";
+import "../../globals.css";
+
+
 export default function RootLayout({
     children,
   }: {
@@ -5,7 +9,11 @@ export default function RootLayout({
   }) {
     return (
       <html lang="en">
-        <body>{children}</body>
+        <body>
+          <Providers>
+            {children}
+          </Providers>
+        </body>
       </html>
     );
   }
