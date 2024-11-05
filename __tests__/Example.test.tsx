155c52ab04324534ac39836dd946223b51e2a0e4
CodyKoInABox <codykoinabox@gmail.com>
2024-11-05 15:09:22 -0300

    create basic example test for product page

--- __tests__/Example.test.tsx
+++ __tests__/Example.test.tsx
@@ -1,16 +1,45 @@
-import {render, screen } from '@testing-library/react'
-import Example from '@/app/(Store)/produto/[produtoURL]/page'
+import { render, screen, waitFor } from '@testing-library/react';
+import Example from '@/app/(Store)/produto/[produtoURL]/page';
 
-it('should have Informações text', () => {
+// mock fetch function
+global.fetch = jest.fn(() =>
+  Promise.resolve({
+    ok: true,
+    json: () => Promise.resolve({
+      breadcrumbs: [{ id: '1', href: '/home', name: 'Home' }],
+      href: '/product',
+      nome: 'Produto',
+      preco: 100,
+      descricao: 'Descrição do produto',
+      sol: 'Meia-sombra',
+      sol_desc: 'Requer meia-sombra',
+      agua: 'Moderada',
+      agua_desc: 'Requer rega moderada',
+      tamanho: 'Pequeno',
+      tamanho_desc: 'Ideal para espaços pequenos',
+      poda: 'Anual',
+      poda_desc: 'Pode ser podado uma vez ao ano',
+      solo: 'Argiloso',
+      solo_desc: 'Ideal para solo argiloso',
+      delicadeza: 'Alta',
+      delicadeza_desc: 'Manuseio delicado',
+      cores: ['Verde'],
+      tamanhos: ['Pequeno', 'Médio', 'Grande']
+    }),
+  })
+) as jest.Mock;
 
-    // Arrange
-    render(<Example params={{
-        produtoURL: ''
-    }} />)
+it('should have Informações text', async () => {
 
-    // Act
-    const myElement = screen.getByText('Informações')
+  // Arrange
+  render(<Example params={{ produtoURL: '' }} />);
 
-    // Assert
-    expect(myElement).toBeInTheDocument()
-})
+
+  // Act
+  await waitFor(() => {
+    const myElement = screen.getByText('Informações');
+
+    //Assert
+    expect(myElement).toBeInTheDocument();
+  });
+});
