88f2fbef75dd2767e6a8792f27bcdce3ad75e966
ManfredHair <manfredhair@gmail.com>
2024-11-11 20:11:50 -0300

    first version of file upload

--- .terraform.lock.hcl
+++ .terraform.lock.hcl
@@ -5,6 +5,7 @@ provider "registry.terraform.io/kreuzwerker/docker" {
   version     = "2.24.0"
   constraints = "~> 2.24.0"
   hashes = [
+    "h1:1z0/qA77T3PS/1m4vRO8UgWjHjk5/v+f3JfGbMyzX18=",
     "h1:fAlQDQgt8oup+qnzH7cg0GSrSP7DgDBcA/XTgFwC9Kk=",
     "zh:181fefd55c8eb75efe9815c43fdd76422b57951ef53b5d5f19273a00fdf0e2e2",
     "zh:2ec84e029d169f188be2addf7f45c2555f226f67d4b6fb66c1749ed5b2c4a76a",
---
+++ src/app/(API)/api/upload/image/route.ts
@@ -0,0 +1,21 @@
+import { NextResponse } from "next/server";
+import { revalidatePath } from "next/cache";
+import fs from "node:fs/promises";
+
+export async function POST(req: Request) {
+  try {
+    const formData = await req.formData();
+
+    const file = formData.get("file") as File;
+    const arrayBuffer = await file.arrayBuffer();
+    const buffer = new Uint8Array(arrayBuffer);
+    await fs.writeFile(`./public/images/${file.name}`, buffer);
+
+    revalidatePath("/");
+
+    return NextResponse.json({ status: "success" });
+  } catch (e) {
+    console.error(e);
+    return NextResponse.json({ status: "fail", error: e });
+  }
+}
